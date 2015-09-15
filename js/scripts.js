b7b772006a7e2531058da2c6fc23c5e64a312774
Kellen Kolbeck <kellengkolbeck@gmail.com>
2015-09-14 23:53:31 -0700

    Refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 function Contact(firstName, lastName) {
-  this.firstName = firstName
-  this.lastName = lastName
-  this.addresses = addresses
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses = addresses;
 }
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + " " + this.city + " " + this state;
+}
+
+function resetFields(){
+  $("input#new-first-name").val()
+  $("input#new-last-name").val()
+  $("input#street").val()
+  $("input#city").val()
+  $("input#state").val()
+  $("div.new-address").not(':first').remove()
 }
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -8,6 +8,20 @@ describe('Contact', function() {
 
   it("adds the fullName method to all contacts", function() {
     var testContact = new Contact("Nelson", "Willie");
-    expect(testContact.fullName).to.equal("Willie Nelson")
+    expect(testContact.fullName()).to.equal("Willie Nelson")
+  });
+});
+
+describe('Address', function() {
+  it("creates a new address with the given specifications", function() {
+    var testAddress = new Address("123 Main Street", "Test City", "Test State");
+    expect(testAddress.street).to.equal("123 Main Street");
+    expect(testAddress.city).to.equal("Test City");
+    expect(testAddress.state).to.equal("Test State");
+  });
+
+  it("adds the fullAddress method to all addresses", function() {
+    var testAddress = new Address("123 Main Street", "Test City", "Test State");
+    expect(testAddress.fullAddress())to.equal("123 Main Street, Test City, Test State")
   });
 });
